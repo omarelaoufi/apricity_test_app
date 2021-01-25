492bc0930f11e2cd3b11896f56a550cc2a58f251
unknown <omar.elaoufi@gmail.com>
2021-01-25 07:04:41 +0100

    [STYLE] Widened and centered table

--- client/src/components/DataTable.js
+++ client/src/components/DataTable.js
@@ -3,7 +3,7 @@ import DataTableHeader from "./DataTableHeader";
 
 export default function DataTable({ data }) {
   return (
-    <table>
+    <table style={{ margin: "auto", width: "60%" }}>
       <DataTableHeader headers={Object.keys(data[0])} />
       <DataTableBody data={data} />
     </table>
