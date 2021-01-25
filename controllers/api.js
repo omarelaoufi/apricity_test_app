6d361e46c1b84b88631c9f94995790b4751b9a08
unknown <omar.elaoufi@gmail.com>
2021-01-25 03:38:48 +0100

    [FEATURE] Added variable data route

--- controllers/api.js
+++ controllers/api.js
@@ -1,10 +1,20 @@
 const MyDatabase = require("../utils/dbHandler");
-const { allQuery } = require("../utils/queries");
+const { allQuery, columnsQuery } = require("../utils/queries");
 
 exports.getAll = (req, res, next) => {
   const db = new MyDatabase("./us-census.db");
 
   db.fetch(allQuery).then((data) => res.json(data));
+
+  db.close();
+};
+
+exports.getVariableData = (req, res, next) => {
+  let variable = req.params.variable;
+
+  const db = new MyDatabase("./us-census.db");
+
+  db.fetch(columnsQuery([variable])).then((data) => res.json(data));
 
   db.close();
 };
--- routes/api.js
+++ routes/api.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const apiCtrl = require("../controllers/api");
 
 router.get("/", apiCtrl.getAll);
+router.get("/:variable", apiCtrl.getVariableData);
 
 module.exports = router;
